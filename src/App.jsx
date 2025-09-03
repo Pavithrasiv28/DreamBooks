96c062c39a6bab50dbd1ce684b873d5bf3888013
Pavithrasiv Softzia <pavithrasivakumar.softzia@gmail.com>
2025-09-03 14:26:50 +0530

    admin_layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import LoginPage from './pages/auth/login/login.jsx'
+import AdminLayout from './layout/admin_layout/layout.jsx'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <LoginPage/>
+      {/* <LoginPage/> */}
+      <AdminLayout/>
     </>
   )
 }
---
+++ src/layout/admin_layout/layout.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { useState } from "react";
+import logo from '../../../public/dream-book-logo.png'
+import LogoutIcon from '@mui/icons-material/Logout';
+import WindowIcon from '@mui/icons-material/Window';
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import PermContactCalendarIcon from '@mui/icons-material/PermContactCalendar';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import Button from '@mui/material/Button';
+
+function AdminLayout() {
+   const [active, setActive] = useState("Dashboard");
+   const menuItems = [
+    { name: "Dashboard", icon: <WindowIcon fontSize="small" /> },
+    { name: "Books", icon: <MenuBookIcon fontSize="small" /> },
+    { name: "Authors", icon: <PermContactCalendarIcon fontSize="small" /> },
+    { name: "Employees", icon: <PeopleAltIcon fontSize="small" /> },
+  ];
+
+  return (
+    <>
+    <div className='layout_container h-[100dvh] w-[100dvw] bg-green-300'>
+
+      {/* Admin Layout */}
+      <div className='sidebar_layout bg-white w-[22dvw] h-[100dvh] p-[min(1em,8%)] box-border'>
+         <div className='dashboard_logo h-2/12 w-full flex items-center justify-center'>
+             <img src={logo} className="max-w-40 h-auto object-contain" />
+         </div>
+         <div className="h-7/12 w-full box-border py-5.5">
+  {menuItems.map((item) => (
+    <Button
+      key={item.name}
+      startIcon={item.icon}
+      onClick={() => setActive(item.name)}
+      sx={{
+        height: "45px",
+        borderRadius: "12px",
+        justifyContent: "flex-start",
+        px: "20px",
+        mb: "4px",
+        fontWeight: 500,
+        textTransform: "none",
+        bgcolor: active === item.name ? "rgb(93 96 239 / var(--tw-bg-opacity, 1))" : "transparent",
+        color: active === item.name ? "white" : "#57564F",
+        "&:hover": {
+          bgcolor: active === item.name ? "indigo.600" : "grey.100",
+        },
+      }}
+      fullWidth
+    >
+      {item.name}
+    </Button>
+  ))}
+</div>
+         <div className='h-3/12 w-full flex items-end justify-center'>
+           <Button variant="text" className='custom w-full' sx={{height:'45px', "& fieldset": { border: "none" }, backgroundColor:'#EEEEEE', color:'#44444E', textAlign:'start', justifyContent: "flex-start", px: "20px",}}><LogoutIcon fontSize='small'/>&nbsp;&nbsp;Logout</Button>
+         </div>
+      </div>
+
+      {/* Routes */}
+      <div className='Route_pages'>
+
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default AdminLayout
